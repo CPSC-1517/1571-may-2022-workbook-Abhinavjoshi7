fd9c0622a27f730764929d5e2cee36321aa92a14
Abhinavjoshi7 <92068770+Abhinavjoshi7@users.noreply.github.com>
2022-05-11 19:23:24 -0600

    test changes

---
+++ Class1/Class1/SupervisoryLevel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview.Data
+{
+    public enum SupervisoryLevel
+    {
+        //default internal values 
+        Entry,  //0
+        TeamMember,//1
+        TeamLeader,//2
+        Supervisor,//3
+        DepartmentHead, //4
+        Owner //5
+    }
+}
